e03dcca31f797348041b04178cd0e1c9ef0ed09a
Klaudia Król <100020626+KlaudiaK0205@users.noreply.github.com>
2022-08-05 14:33:09 +0200

    Zadanie 1
    DONE

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let randomColor = null;
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    randomColor = setInterval(() => {
+        const color = getRandomHexColor();
+        
+        body.style.background = color;
+    }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    
+    clearInterval(randomColor);
+});
+
+// DONE
