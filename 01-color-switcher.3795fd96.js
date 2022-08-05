1feb20e80461fcbc19552b33c7726ec79deb5b78
KlaudiaK0205 <100020626+KlaudiaK0205@users.noreply.github.com>
2022-08-05 12:33:36 +0000

    Deploying to gh-pages from @ KlaudiaK0205/goit-js-hw-09@e03dcca31f797348041b04178cd0e1c9ef0ed09a 🚀

---
+++ 01-color-switcher.3795fd96.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");var n=null;t.addEventListener("click",(function(){t.disabled=!0,a.disabled=!1,n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));e.style.background=t}),1e3)})),a.addEventListener("click",(function(){t.disabled=!1,a.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.3795fd96.js.map
