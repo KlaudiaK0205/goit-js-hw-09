1e7e79bd3670e6a5bc6b00db28080050d30d8663
Klaudia Król <100020626+KlaudiaK0205@users.noreply.github.com>
2022-08-06 09:05:20 +0200

    Zadanie 3
    DONE

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix, {Notify} from 'notiflix';
+
+const createBtn = document.querySelector('button');
+const form = document.querySelector('.form');
+
+const createPromise = (position, delay) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  })
+  return promise;
+};
+
+createBtn.addEventListener('click', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const delaySteps = Number(form.elements.step.value);
+  const amount = form.elements.amount.value;
+
+  for (let i = 1; i <= amountInput; i++){
+    let newTime = 0;
+    const actualTime = sumTime(newTime, delayInput, delayStepsInput, i);
+    createPromise(i, actualTime)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`)
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`)
+      })
   }
-}
+});
+
+// DONE
